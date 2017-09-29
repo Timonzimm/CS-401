b1fc5cc54191f83a7adb727da124aa051b8d5aaa
TZimmermann <timon@thor.cambridge-us1089.slb.com>
2017-09-29 09:52:45 -0400

    Date regex

--- HW01/loader.py
+++ HW01/loader.py
@@ -1,13 +1,16 @@
-import glob, os
+import glob, os, re
 import pandas as pd
 
 DATA_FOLDER = 'Data'
 os.chdir('./{}/ebola'.format(DATA_FOLDER))
 DIRS_COUNTRIES = [d for d in os.listdir('.') if os.path.isdir(os.path.join('.', d))]
+DATE_PATTERN = re.compile(r'([0-9]{4})-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])')
 
 for dir in DIRS_COUNTRIES:
     table_files = glob.glob("./{}/*.csv".format(dir))
 
     for f in table_files:
-        df = pd.read_csv(f)
-        print(df.columns)
+        year, month, day = DATE_PATTERN.search(f).groups()
+        print(year, month, day)
+        #df = pd.read_csv(f)
+        #print(df.columns)
