9dbc856d47d6dc917cf4fe56c1722a5dfc11deab
TZimmermann <timon@thor.cambridge-us1089.slb.com>
2017-09-29 09:56:55 -0400

    .

--- HW01/loader.py
+++ HW01/loader.py
@@ -6,11 +6,15 @@ os.chdir('./{}/ebola'.format(DATA_FOLDER))
 DIRS_COUNTRIES = [d for d in os.listdir('.') if os.path.isdir(os.path.join('.', d))]
 DATE_PATTERN = re.compile(r'([0-9]{4})-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])')
 
+dfs = []
 for dir in DIRS_COUNTRIES:
     table_files = glob.glob("./{}/*.csv".format(dir))
-
+    print('Country: {}'.format(dir))
+    df = pd.DataFrame()
     for f in table_files:
         year, month, day = DATE_PATTERN.search(f).groups()
         print(year, month, day)
-        #df = pd.read_csv(f)
-        #print(df.columns)
+        df = pd.read_csv(f)
+        print(df.columns)
+    print('*'*50)
+    exit()
