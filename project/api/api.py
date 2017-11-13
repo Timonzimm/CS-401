11b93ae2e3ff4c4e93d66d97487f31843f9bd0fc
joelmfonseca <joel.fonseca@epfl.ch>
2017-11-13 23:21:58 +0100

    added api functions for pop-up window

--- project/api/api.py
+++ project/api/api.py
@@ -18,23 +18,66 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
-@app.route('/countries')
-def all_attacks_by_country():
-    cur = get_db().execute('SELECT iso_code, COUNT(*) as num_attacks FROM Attacks GROUP BY iso_code')
-    num_attacks = cur.fetchall()
-    cur.close()
-    return jsonify(num_attacks)
-
 @app.route('/coords/<string:country>')
-def all_attacks_by_coordinates_sample(country):
+def attack_coordinates_by_country(country):
+    """Returns the coordinates of all attacks of the given country."""
     cur = get_db().execute('SELECT longitude, latitude FROM Attacks WHERE iso_code="{}"'.format(country))
     num_attacks = cur.fetchall()
     cur.close()
     return jsonify(num_attacks)
 
 @app.route('/coords/year/<int:year>')
-def all_attacks_by_coordinates_and_year(year):
+def attack_coordinates_by_year(year):
+    """Returns the coordinates of all attacks of the given year."""
     cur = get_db().execute('SELECT longitude, latitude FROM Attacks WHERE iyear={}'.format(year))
+    num_attacks = cur.fetchall()
+    cur.close()
+    return jsonify(num_attacks)
+
+@app.route('/attacks/countries')
+def all_attacks():
+    """Returns the total number of attacks for each country."""
+    cur = get_db().execute('SELECT iso_code, COUNT(*) as num_attacks FROM Attacks GROUP BY iso_code')
+    num_attacks = cur.fetchall()
+    cur.close()
+    return jsonify(num_attacks)
+
+@app.route('/attacks/types/<string:country>')
+def attack_types_by_country(country):
+    """Returns the types list with the corresponding number of attacks in descending order of the given country."""
+    cur = get_db().execute('SELECT attacktype1_txt, num_attacks FROM (SELECT attacktype1_txt, COUNT(attacktype1_txt) num_attacks FROM Attacks WHERE iso_code="{}" GROUP BY attacktype1_txt) ORDER BY num_attacks DESC'.format(country))
+    num_attacks = cur.fetchall()
+    cur.close()
+    return jsonify(num_attacks)
+
+@app.route('/attacks/targets/<string:country>')
+def attack_targets_by_country(country):
+    """Returns the targets list with the corresponding number of attacks in descending order of the given country."""
+    cur = get_db().execute('SELECT targtype1_txt, num_attacks FROM (SELECT targtype1_txt, COUNT(targtype1_txt) num_attacks FROM Attacks WHERE iso_code="{}" GROUP BY targtype1_txt) ORDER BY num_attacks DESC'.format(country))
+    num_attacks = cur.fetchall()
+    cur.close()
+    return jsonify(num_attacks)
+
+@app.route('/attacks/perpetrators/<string:country>')
+def attack_perpetrators_by_country(country):
+    """Returns the perpetrators list with the number of victims corresponding to their attacks in descending order of the given country."""
+    cur = get_db().execute('SELECT gname, num_victims FROM (SELECT gname, SUM(nkill) num_victims FROM Attacks WHERE iso_code="{}" GROUP BY gname) ORDER BY num_victims DESC LIMIT 20'.format(country))
+    num_attacks = cur.fetchall()
+    cur.close()
+    return jsonify(num_attacks)
+
+@app.route('/attacks/num_victims/<string:country>')
+def num_victims_per_year_by_country(country):
+    """Returns the number of victims per year of the given country."""
+    cur = get_db().execute('SELECT iyear, SUM(nkill) FROM Attacks WHERE iso_code="{}" GROUP BY iyear'.format(country))
+    num_attacks = cur.fetchall()
+    cur.close()
+    return jsonify(num_attacks)
+
+@app.route('/attacks/num_attacks/<string:country>')
+def num_attacks_per_year_by_country(country):
+    """Returns the number of attacks per year of the given country."""
+    cur = get_db().execute('SELECT iyear, COUNT(*) FROM Attacks WHERE iso_code="{}" GROUP BY iyear'.format(country))
     num_attacks = cur.fetchall()
     cur.close()
     return jsonify(num_attacks)
