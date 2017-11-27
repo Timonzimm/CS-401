f9823bd8b2ade203f6ab6e8bcc9f696e23e20901
Timon <timonzimm@gmail.com>
2017-11-27 15:04:38 +0100

    GIFs for the report

--- project/website/src/map.js
+++ project/website/src/map.js
@@ -19,7 +19,7 @@ $('#charts-modal').modal({
     ready: () => details_chart.resizeListener()
 })
 
-API_SERVER = "http://128.179.139.248:5000"
+API_SERVER = "http://127.0.0.1:5000"
 
 // DEFINE VARIABLES
 // Define size of map group
